a0ecb3fd82ed3e5c86f642b2e01ff7373279673e
Vasy <taki003@yandex.ru>
2025-01-31 00:22:53 +0700

    saves to save

--- mysite/recipe/views.py
+++ mysite/recipe/views.py
@@ -2,7 +2,7 @@ from timeit import default_timer
 from django.contrib.auth.models import Group
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Order
 
 
 def recipe_index(request: HttpRequest):
@@ -28,5 +28,12 @@ def groups_list(request: HttpRequest):
 
 
 def products_list(request: HttpRequest):
-    context = {"products": Product.objects.all(),}
+    context = {"products": Product.objects.all(), }
     return render(request, 'recipe/products-list.html', context=context)
+
+
+def orders_list(request: HttpRequest):
+    context = {"orders": Order.objects.select_related("user").prefetch_related("products").all(),
+
+               }
+    return render(request, 'recipe/orders-list.html', context=context)
