2e50e54a02b326298c3ab89a909b59b5db39c8cf
Vasy <taki003@yandex.ru>
2025-02-11 13:48:27 +0700

    new adds+request

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'recipe.apps.RecipeConfig',
+    'requestdataapp.apps.RequestdataappConfig',
 ]
 
 MIDDLEWARE = [
---
+++ mysite/requestdataapp/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import process_get_view
+app_name = 'requestdataapp'
+urlpatterns = [
+    path("get/", process_get_view, name="get-vew")
+]
